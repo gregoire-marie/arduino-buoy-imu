fe24a28bb28f8d0fb5c5570cc0ceccb3ef06a727
anosen <gregoire.marie08@gmail.com>
2019-06-30 09:57:05 -0400

    Added 3D spatial plot

--- app/plot_serial.py
+++ app/plot_serial.py
@@ -3,7 +3,8 @@ import threading
 import matplotlib
 
 from src.main.python.plot.plot_utils import (
-    initialize_2d_plot, update_2d_plot
+    initialize_2d_raw_plot, update_2d_raw_plot, initialize_2d_angles_plot, update_2d_angles_plot, initialize_3d_plot,
+    update_3d_plot
 )
 from src.main.python.plot.save_utils import setup_csv, update_csv
 from src.main.python.serial.read_serial import ProcessedSerialReader
@@ -27,7 +28,9 @@ serial_reader = ProcessedSerialReader(port=SERIAL_PORT, baud_rate=115200, timeou
 serial_reader.connect()
 
 # Setup raw data figure
-fig_2d, axs_2d, lines_2d = initialize_2d_plot()
+fig_2d_raw, axs_2d_raw, lines_2d_raw = initialize_2d_raw_plot()
+fig_2d_angles, axs_2d_angles, lines_2d_angles = initialize_2d_angles_plot()
+fig_3d, axs_3d, cube, cube_vertices, cube_axes = initialize_3d_plot()
 
 # Setup CSV file
 csv_path = f"{RESULTS_FOLDER}/serial/sensor_data.csv"
@@ -38,8 +41,14 @@ if csv_bool:
     csv_updater = threading.Thread(target=update_csv, args=(serial_reader, csv_path), daemon=True).start()
 
 # Start animations
-ani_2d = animation.FuncAnimation(fig_2d, update_2d_plot, interval=1, cache_frame_data=False,
-                                 fargs=(serial_reader, lines_2d, axs_2d))
+ani_2d_raw = animation.FuncAnimation(fig_2d_raw, update_2d_raw_plot, interval=100, cache_frame_data=False,
+                                     fargs=(serial_reader, lines_2d_raw, axs_2d_raw))
+#
+ani_2d_angles = animation.FuncAnimation(fig_2d_angles, update_2d_angles_plot, interval=100, cache_frame_data=False,
+                                     fargs=(serial_reader, lines_2d_angles, axs_2d_angles))
+
+ani_3d = animation.FuncAnimation(fig_3d, update_3d_plot, interval=100, cache_frame_data=False,
+                                 fargs=(serial_reader, axs_3d, cube, cube_vertices, cube_axes))
 
 # Show plots
 plt.show()
